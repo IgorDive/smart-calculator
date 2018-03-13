27bd0b097a49469980c629c668780e8c255b15fc
IgorDive <ihar.kazlovich@gmail.com>
2018-03-13 03:08:43 +0300

    fixed bugs src/index.js

--- src/index.js
+++ src/index.js
@@ -3,29 +3,98 @@ class SmartCalculator {
     this._value = [initialValue];
     this._tempArr = [];
     this._result;
+    this._tempValue;
+    this._multiply;
     this.__proto__.toString = () => this._result;
 
     this._solver =  function() {
       this._tempArr = [];
       this._value.reduceRight( (prevValue, item, i, arr) => {
         switch( item ) {
-          case '*': return arr[i - 1] * prevValue;
+          case '*':
+            if ( this._tempValue ) {
+              if ( this._multiply ) {
+                prevValue *= this._tempValue;
+                this._tempValue = 0; 
+              } else {
+                prevValue /= this._tempValue;
+                this._tempValue =0;
+              };
+            };
+
+            return arr[i - 1] * prevValue;
           break;
-          case '/': return arr[i - 1] / prevValue;
+          case '/': 
+            if ( this._tempValue ) {
+              if ( this._multiply ) {
+                prevValue *= this._tempValue;
+                this._tempValue = 0; 
+              } else {
+                prevValue /= this._tempValue;
+                this._tempValue =0;
+              };
+            };
+
+            return arr[i - 1] / prevValue;
           break;
-          case '^': return Math.pow(arr[i - 1], prevValue);
+          case '^': 
+            if ( arr[i + 2] === '*' ) {
+              this._multiply = true;
+              this._tempValue = prevValue / arr[i + 1];
+              prevValue = arr[i + 1];
+            };
+            if ( arr[i + 2] === '/' ) {
+              this._multiply = false;
+              this._tempValue = arr[i + 1] / prevValue;
+              prevValue = arr[i + 1];
+            }
+            
+            return Math.pow(arr[i - 1], prevValue);
           break;
-          case '+': 
+          case '+':
+            if ( this._tempValue ) {
+              if ( this._multiply ) {
+                prevValue *= this._tempValue;
+                this._tempValue = 0; 
+              } else {
+                prevValue /= this._tempValue;
+                this._tempValue =0;
+              };
+            };
+          
             this._tempArr.push(prevValue);
             return arr[i - 1];
           break;
           case '-': 
+            if ( this._tempValue ) {
+              if ( this._multiply ) {
+                prevValue *= this._tempValue;
+                this._tempValue = 0; 
+              } else {
+                prevValue /= this._tempValue;
+                this._tempValue =0;
+              };
+            };
+
             this._tempArr.push(-prevValue);
             return arr[i - 1];
           break;
           default:
-          if ( arr[i - 1] === undefined ) this._tempArr.push(prevValue); 
-          return prevValue;
+            if ( arr[i - 1] === undefined ) {
+              if ( this._tempValue ) {
+                if ( this._multiply ) {
+                  prevValue *= this._tempValue;
+                  this._tempValue = 0; 
+                } else {
+                  prevValue /= this._tempValue;
+                  this._tempValue =0;
+                };
+              };
+
+              this._tempArr.push(prevValue)
+            } else { 
+            return prevValue;
+            };
         }
       } );
       
